99653fc4d7fb81b161201b5be6687a3dd6941c2b
ahmedhindi <ahmedelsyd5@gmail.com>
2017-04-13 10:26:57 +0200

    "init lda"

--- pytext/LDA.py
+++ pytext/LDA.py
@@ -6,6 +6,7 @@ import numpy as np
 import pyLDAvis.gensim
 import pyLDAvis
 
+
 class LDA():
     """
     this cless makes, saves to desk, and loads
@@ -20,15 +21,17 @@ class LDA():
         text_data= pandas cleaned text (optional)
         vis= pyLDAvis.display objec (optional)
     """
-    def __init__(self, data=None, model=None, corpus=None, dictionary=None, text_data=None, vis=None):
-        self.data = data
-        self.model = model
-        self.corpus = corpus
-        self.dictionary = dictionary
-        self.text_data = text_data
-        self.vis = vis
+
+    def __init__(self):
+        self.data = None
+        self.model = None
+        self.corpus = None
+        self.dictionary = None
+        self.text_data = None
+        self.vis = None
 
     "________________________DICTIONARY______________________"
+
     def make_dict(self):
         """takes pandas series and returns dictionary to use with gensim   """
         text_data = self.data.apply(lambda i: str(i).split(' '))
@@ -36,7 +39,7 @@ class LDA():
         self.dictionary = dictionary
         self.text_data = text_data
 
-    def save_dict(self, dict_name = 'dict'):
+    def save_dict(self, dict_name='dict'):
         """saves the dictionary to the disk
             dict_name --> dictionary path + filename
         """
@@ -47,21 +50,23 @@ class LDA():
         self.dictionary = dictionary
 
     "________________________CORPUS___________________________"
+
     def make_corpus(self):
         """ makes a corpus from gensim.corpora.dictionary object """
         corpus = [self.dictionary.doc2bow(text) for text in self.text_data]
         self.corpus = corpus
 
     def save_corpus(self, corpus_name='corpus'):
-        self.corpus.dump( self.corpus, open( "{}.pkl".format(corpus_name), "wb" ))
+        self.corpus.dump(self.corpus, open("{}.pkl".format(corpus_name), "wb"))
 
     def load_corpus(corpus_name='corpus'):
-        self.corpus = pickle.load(open( "{}.pkl".format(corpus_name), "rb" ))
+        self.corpus = pickle.load(open("{}.pkl".format(corpus_name), "rb"))
 
     "________________________MODEL____________________________"
-    def make_model(self,num_top=10):
+
+    def make_model(self, num_top=10):
         self.model = gensim.models.ldamodel.LdaModel(self.corpus, num_topics=num_top,
-                                               id2word= self.dictionary, passes = 15)
+                                                     id2word=self.dictionary, passes=15)
 
     def save_model(self, model_name='model'):
         self.model.save('{}.gensim'.format(model_name))
@@ -70,6 +75,8 @@ class LDA():
         self.model = gensim.models.ldamodel.LdaModel.load('{}.gensim'.format(model_name))
 
     "________________________VIS_____________________________"
+
     def make_vis(self, sort_topics=True):
-        lda_display = pyLDAvis.gensim.prepare(self.model,self.corpus,self.dictionary,sort_topics=sort_topics)
+        lda_display = pyLDAvis.gensim.prepare(
+            self.model, self.corpus, self.dictionary, sort_topics=sort_topics)
         self.vis = pyLDAvis.display(lda_display)
